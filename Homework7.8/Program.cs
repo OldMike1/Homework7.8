21421290f28421a8b05af9d1063eecce975d88b1
mike19952006 <mike19952006@gmail.com>
2018-03-03 01:09:42 +0200

    Вывести из массива отдельно четные и нечетные числа.

--- Homework7.8/Program.cs
+++ Homework7.8/Program.cs
@@ -10,6 +10,25 @@ namespace Homework7._8
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Number of elements in the array");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            Random rnd = new Random();
+
+            Console.Write("Even numbers: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 2);
+                if (array[i] == 0)
+                    Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Odd numbers: ");
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] != 0)
+                    Console.Write(array[i]+" ");
+            
         }
     }
 }
